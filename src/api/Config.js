fb2abba081ae2596b73ced76efd800f659c1e828
sachin888 <sachinitynitp2k8@gmail.com>
2019-02-17 10:23:47 +0530

    read key from .env file

--- src/api/Config.js
+++ src/api/Config.js
@@ -1,7 +1,6 @@
 
-//export let GOODREADS_API_KEY = process.env.GOODREADS_API_KEY;
-export const GOODREADS_API_KEY = 'SPRIXjIlsJben87exo5lEg';
-//GOODREADS_API_KEY = 'SPRIXjIlsJben87exo5lEg';
+export let GOODREADS_API_KEY = process.env.GOODREADS_API_KEY;
+
 export const API_SETTINGS_CONFIG = {
     headers: {
         'X-Requested-With': 'XMLHttpRequest' // Tell server that it is an AJAX request.
